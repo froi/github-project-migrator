ac3beb0a94560c43d849efc14e6e87b679306a05
Froilan Irizarry Rivera <1918027+froi@users.noreply.github.com>
2020-05-11 01:47:33 -0400

    Add new interfaces and types and update response interfaces
    Added new response interfaces. These are used to handle the returned results from the GitHub API,
    Changed from filed types to work better with the GitHub API
    Added new defined types
    
    Signed-off-by: Froilan Irizarry Rivera <1918027+froi@users.noreply.github.com>

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,46 +1,150 @@
-import { RequestParameters } from '@octokit/types';
+import { RequestParameters } from "@octokit/types";
 
-export interface GetOrgProjectResponse {
-    organization: {
-        project: {
-            id: string;
-            name: string;
-        }
-    }
+export type HTML = string;
+export type ID = string;
+export type URI = string;
+export type DateTime = Date;
+
+// export interface ProjectCard {
+//     column: ProjectColumn;
+//     content: ProjectCardItem;
+//     createdAt: DateTime;
+//     creator: Actor;
+//     databaseId: number;
+//     id: ID;
+//     isArchived: Boolean;
+//     note: String;
+//     project: Project;
+//     resourcePath: URI;
+//     state: ProjectCardState;
+//     updatedAt: DateTime;
+//     url: URI;
+// }
+export interface ProjectColumn {
+  createdAt: DateTime;
+  databaseId: number;
+  id: ID;
+  name: string;
+  project: Project;
+  purpose: ProjectColumnPurpose;
+  resourcePath: URI;
+  updatedAt: DateTime;
+  url: URI;
+  cards: [];
 }
-interface RepoProjectCard {
-    content: {
-        __typename: string;
-        id: string;
-        number: number;
-    }
+export interface Actor {
+  login: string;
+  resourcePath: URI;
+  url: URI;
 }
-interface RepoProjectColumns {
-    name: string;
-    cards: {
-        nodes: RepoProjectCard[];
-    }
+export interface Project {
+  body: String;
+  bodyHTML: HTML;
+  closed: boolean;
+  closedAt: DateTime;
+  createdAt: DateTime;
+  creator: Actor;
+  databaseId: number;
+  id: ID;
+  name: string;
+  number: number;
+  owner: ProjectOwner;
+  resourcePath: URI;
+  state: ProjectState;
+  updatedAt: DateTime;
+  url: URI;
+  viewerCanUpdate: boolean;
 }
+export interface ProjectOwner {
+  id: ID;
+  projectsResourcePath: URI;
+  projectsUrl: URI;
+  viewerCanCreateProject: boolean;
+}
+export enum ProjectState {
+  CLOSED = "CLOSED",
+  OPEN = "OPEN",
+}
+export enum ProjectColumnPurpose {
+  DONE = "DONE",
+  IN_PROGRESS = "IN_PROGRESS",
+  TODO = "TODO",
+}
+export interface ProjectCard {
+  note: string;
+  content: {
+    __typename: string;
+    id: ID;
+    number: number;
+  };
+}
+export interface RepoProjectColumns {
+  name: string;
+  cards: {
+    nodes: ProjectCard[];
+  };
+}
+
 export interface GetRepoProjectResponse {
-    repository: {
-        project: {
-            name: string;
-            columns: {
-                nodes: RepoProjectColumns[];
-            }
-        }
-    }
+  repository: {
+    project: {
+      id: ID;
+      name: string;
+      columns: {
+        nodes: RepoProjectColumns[];
+      };
+    };
+  };
+}
+export interface GetOrgProjectResponse {
+  organization: {
+    project: {
+      id: ID;
+      name: string;
+    };
+  };
+}
+export interface AddProjectColumnResponse {
+  addProjectColumn: {
+    columnEdge: {
+      node: {
+        id: ID;
+        name: string;
+      };
+    };
+    project: {
+      id: ID;
+    };
+  };
+}
+export interface AddProjectCardResponse {
+  addProjectCard: {
+    cardEdge?: {
+      node: {
+        id: ID;
+      };
+    };
+    projectColumn?: {
+      id: ID;
+    };
+  };
+}
+export interface AddProjectCardInput extends RequestParameters {
+  clientMutationId?: string;
+  contentId?: ID | null;
+  note?: string | null;
+  projectColumnId: ID;
 }
 export interface AddProjectColumnInput extends RequestParameters {
-    projectId: number;
-    columnName: string;
+  projectId: ID;
+  columnName: string;
 }
 export interface GetOrgProjectInput extends RequestParameters {
-    org: string;
-    projectNumber: number;
+  org: string;
+  projectNumber: number;
 }
 export interface GetRepoProjectInput extends RequestParameters {
-    owner: string;
-    repo: string;
-    projectNumber: string;
+  owner: string;
+  repo: string;
+  projectNumber: number;
 }
