9ca7811d537d7ac4a9bbcb0b84d9f59004a3ca16
Froilan Irizarry Rivera <1918027+froi@users.noreply.github.com>
2020-11-28 16:52:41 -0500

    Remove graphql queries and use graphql query files
    I've removed the graphql query strings in favor of using the new graphql query files. This will help us keep this code cleaner.
    
    I've also imported the new utils module and the new GraphqlQueries enum from the newly renamed libs/types.ts.

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,10 @@ import {
   GetRepoProjectInput,
   GetOrgProjectResponse,
   GetRepoProjectResponse,
-  ID
-} from './interfaces';
+  ID,
+  GraphQlQueries
+} from './libs/types';
+import {getGraphqlQuery} from './libs/utils';
 
 dotenv.config();
 const graphqlWithAuth = graphql.defaults({
@@ -21,80 +23,24 @@ const graphqlWithAuth = graphql.defaults({
 });
 
 async function addProjectColumn(input: AddProjectColumnInput): Promise<AddProjectColumnResponse> {
-  const mutation = `mutation($projectId: ID!, $columnName: String! ) {
-      addProjectColumn(input: { name: $columnName, projectId: $projectId }) {
-        columnEdge {
-          node {
-            id
-            name
-          }
-        }
-        project {
-          id
-        }
-      }
-    }`;
-  const result = (await graphqlWithAuth(mutation, input)) as AddProjectColumnResponse;
+  const mutation = getGraphqlQuery(GraphQlQueries.ADD_PROJECT_COLUMN);
+  const result: AddProjectColumnResponse = await graphqlWithAuth(mutation, input);
   return result;
 }
 async function addProjectCard(input: AddProjectCardInput): Promise<AddProjectCardResponse> {
-  const mutation = `mutation($contentId: ID, $note: String, $projectColumnId: ID! ) {
-    addProjectCard(input: { contentId: $contentId, note: $note, projectColumnId: $projectColumnId }) {
-      cardEdge {
-        node {
-          id
-        }
-      }
-      projectColumn {
-        id
-      }
-    }
-  }`;
-  const result = (await graphqlWithAuth(mutation, input)) as AddProjectCardResponse;
+  const mutation = getGraphqlQuery(GraphQlQueries.ADD_PROJECT_CARD);
+  const result: AddProjectCardResponse = await graphqlWithAuth(mutation, input);
   return result;
 }
 async function getOrgProject(input: GetOrgProjectInput): Promise<GetOrgProjectResponse> {
-  const query = `query ($org: String!, $projectNumber: Int!){
-      organization(login:$org) {
-        project(number:$projectNumber) {
-          id
-          name
-        }
-      }
-    }`;
-  return (await graphqlWithAuth(query, input)) as GetOrgProjectResponse;
+  const query = getGraphqlQuery(GraphQlQueries.GET_ORG_PROJECT);
+  const result: GetOrgProjectResponse = await graphqlWithAuth(query, input);
+  return result;
 }
 async function getRepoProject(input: GetRepoProjectInput): Promise<GetRepoProjectResponse> {
-  const query = `query ($owner: String!, $repo: String!, $projectNumber: Int!){
-      repository(name:$repo, owner:$owner) {
-        project(number:$projectNumber) {
-          name
-          columns(first:100){
-            nodes {
-              name
-              cards(first:100){
-                nodes {
-                  note
-                  content {
-                    __typename
-                    ...on Issue {
-                      id
-                      number
-                    }
-                    ...on PullRequest {
-                      id
-                      number
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }`;
-
-  return (await graphqlWithAuth(query, input)) as GetRepoProjectResponse;
+  const query = getGraphqlQuery(GraphQlQueries.GET_REPO_PROJECT);
+  const result: GetRepoProjectResponse = await graphqlWithAuth(query, input);
+  return result;
 }
 async function main(): Promise<void> {
   const org: string | undefined = process.env.GITHUB_ORG;
